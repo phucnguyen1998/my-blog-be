1f9daf833ff606c5018006de1af88f14c1396371
phucnguyen1998 <phucnguyen11011998@gmail.com>
2021-01-26 13:59:20 +0700

    Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a34e1c3bdad808fb2ce8db17303aac5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59fba1aa-06d9-4f6b-ac5f-9f13ba7e0f44'
+};
